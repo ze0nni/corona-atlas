18a9f177dda42b1b4575138ff0a5d0506024eac0
e.blagodarev <ze0nni@gmail.com>
2019-07-30 11:34:23 +0300

    command `root` now set path for all atlases

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ Cli.withStdinLines(async function(lines, newline) {
 
 function Context(context) {
 	let outputDirectory = "."
+	let currentRoot = "."
 	let currentAtlas = null
 
 	function WithAtlas(cmd) {
@@ -48,10 +49,10 @@ function Context(context) {
 
 	return {
 		out,
+		root,
 		atlas,
 		buildAtlas,
 		config: WithAtlas(config),
-		root: WithAtlas(NoAtlasPropertys(["root"], root)),
 		size: WithAtlas(NoAtlasPropertys(["width", "height"], size)),
 		put: WithAtlas(put)
 	}
@@ -105,15 +106,14 @@ function Context(context) {
 	}
 
 	async function root(rootPath) {
-		currentAtlas.root = rootPath
+		currentRoot = rootPath
 	}
 
 	async function put(dir) {
-		const root = currentAtlas.root
 		const newImages = []
-		for (let f of Fs.readdirSync(Path.join(root, dir)).filter(isImage)) {
-			const fullName = Path.join(root, dir, f)
-			const relativeName = Path.relative(root, fullName)
+		for (let f of Fs.readdirSync(Path.join(currentRoot, dir)).filter(isImage)) {
+			const fullName = Path.join(currentRoot, dir, f)
+			const relativeName = Path.relative(currentRoot, fullName)
 			newImages.push({
 				name: relativeName,
 				path: fullName
